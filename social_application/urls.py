70877a75199277b3cd3069144f89cdafa478ffd1
Alex_Freud <NikiforenkoAS@i.ua>
2023-09-12 10:35:27 +0300

    Create default viewswets for all serealizers and connect them in default router

--- social_application/urls.py
+++ social_application/urls.py
@@ -1,7 +1,20 @@
 from rest_framework import routers
 
+from social_application.views import (
+    CommentViewSet,
+    PostViewSet,
+    ReactionViewSet,
+    FollowViewSet,
+    ProfileViewSet,
+)
+
 
 router = routers.DefaultRouter()
+router.register("comments", CommentViewSet)
+router.register("posts", PostViewSet)
+router.register("reactions", ReactionViewSet)
+router.register("follows", FollowViewSet)
+router.register("profiles", ProfileViewSet)
 
 urlpatterns = router.urls
 
--- social_application/views.py
+++ social_application/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.db.models import Q, F, Count
+from rest_framework import viewsets
 
-# Create your views here.
+from social_application.models import (
+    Comment,
+    Post,
+    Reaction,
+    Follow,
+    Profile,
+)
+
+from social_application.serializers import (
+    CommentSerializer,
+    PostSerializer,
+    ReactionSerializer,
+    FollowSerializer,
+    ProfileSerializer,
+)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class ReactionViewSet(viewsets.ModelViewSet):
+    queryset = Reaction.objects.all()
+    serializer_class = ReactionSerializer
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
